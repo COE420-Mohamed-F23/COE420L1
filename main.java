6efcbb8bcabb416796000859aba91a91329617cd
d3cimate <dexter@dexters-Laptop.local>
2023-09-06 17:40:53 +0400

    Feature B added

--- main.java
+++ main.java
@@ -1,7 +1,13 @@
 class main
 {
+    static void featureB()
+    {
+	    System.out.println("featureB added!");
+    }
+
     public static void main(String[] args)
     {
-        System.out.println("Hello World");
+        System.out.println("Hello World!");
+        featureB();
     }
 }
